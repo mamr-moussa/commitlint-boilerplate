ba29021d6bf98580f96548385260d6ad3e1dfb12
Amr <muhammad.amro.moussa@gmail.com>
2020-12-14 01:45:29 +0200

    feat(BIT-123): add commitlint  custom rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+		'scope-case': [2, 'always', ['uppercase']]
+	}
+}
