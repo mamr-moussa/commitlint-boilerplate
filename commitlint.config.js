2633c92e4ab7be74c92c5447d3c8734e4fe591ae
Amr <muhammad.amro.moussa@gmail.com>
2020-12-14 02:01:50 +0200

    fix(Lint): Configure jest with eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'subject-case': [2, 'never', ['start-case', 'pascal-case']],
-		'scope-case': [2, 'always', ['upper-case']]
+		'scope-case': [2, 'always', ['upper-case', 'start-case']]
 	}
 }
